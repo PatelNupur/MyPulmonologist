bcd100354a0543e191be8446bfb641eb1f4e0096
PatelNupur <nupur.work131@gmail.com>
2025-03-19 11:41:57 -0400

    send_email_update

--- app.py
+++ app.py
@@ -48,9 +48,9 @@ app.config['MAIL_SERVER'] = 'smtp.gmail.com'  # Use Gmail's SMTP
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USE_SSL'] = False 
-app.config['MAIL_USERNAME'] = 'nupurpatel1301@gmail.com'  # Replace with your email
-app.config['MAIL_PASSWORD'] = 'wjqi rnck egye pirh'  # Use App Password, not your actual password
-app.config['MAIL_DEFAULT_SENDER'] = 'nupurpatel1301@gmail.com'
+app.config['MAIL_USERNAME'] = 'my.pulmonologist.prediction@gmail.com'  # Replace with your email
+app.config['MAIL_PASSWORD'] = 'bloc cnfq lqzo lyoh'  # Use App Password, not your actual password
+app.config['MAIL_DEFAULT_SENDER'] = 'my.pulmonologist.prediction@gmail.com'
 
 mail = Mail(app)  # Initialize Flask-Mail
 
